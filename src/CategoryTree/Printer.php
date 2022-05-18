7fe2f07eb0b8144d2b3363c74d39b9514715c682
Davila Lopez <luis.g.davila.lopez@accenture.com>
2022-05-17 21:30:56 -0500

    done

--- src/CategoryTree/Printer.php
+++ src/CategoryTree/Printer.php
@@ -4,11 +4,23 @@ namespace Kata\CategoryTree;
 
 class Printer
 {
+
+    private function getStringTree(Category $parent, int $spaces = 0){
+        $children = $parent->children();
+        if(empty($children)){
+            return str_repeat('  ',$spaces++).$parent->name().PHP_EOL;
+        }else{
+            $stringTree = str_repeat('  ',$spaces++).$parent->name().PHP_EOL;
+            usort($children, fn($a,$b) => $a->name()<=>$b->name());
+            foreach($children as $child){
+                $stringTree = $stringTree.$this->getStringTree($child, $spaces); 
+            }
+            return $stringTree;
+
+        }
+    }
     public function build(Category $parent, int $spaces = 0): string
     {
-        /**
-         * @todo
-         */
-        return '';
+        return $this->getStringTree($parent);
     }
 }
--- src/UniqueArray/DuplicateRemover.php
+++ src/UniqueArray/DuplicateRemover.php
@@ -4,11 +4,25 @@ namespace Kata\UniqueArray;
 
 class DuplicateRemover
 {
+
+    function inArray(array $array ,mixed $val): bool {
+        for ($i = 0; $i < count($array); $i++){
+            if($array[$i] == $val){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public function __invoke(array $input): array
     {
-        /**
-         * @todo
-         */
-        return [];
+        $noDuplicateArray = [];
+        $conuntArray = 0;
+        for ($i = 0; $i < count($input); $i++){
+            if(!$this->inArray($noDuplicateArray,$input[$i])){
+                $noDuplicateArray[$conuntArray++] =  $input[$i];
+            }
+        }
+        return $noDuplicateArray;
     }
 }
